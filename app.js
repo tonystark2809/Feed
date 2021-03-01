9710ad003f8f03856b8d1a0aa4ab0794ab7cae53
tonystark2809 <73586175+tonystark2809@users.noreply.github.com>
2021-03-01 10:33:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "images");
+	},
+	filename: function (req, file, cb) {
+		cb(null, uuidv4());
+	},
+});
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
+// app.use(bodyParser.urlencoded()); //for form data
+app.use(bodyParser.json());
+app.use(
+	multer({
+		storage: storage,
+		fileFilter: fileFilter,
+	}).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+	.connect(
+		"mongodb+srv://Ayush:ayush2809@cluster0-aukgf.mongodb.net/messages?retryWrites=true&w=majority"
+	)
+	.then((result) => {
+		const server = app.listen(8080);
+		const io = require("socket.io")(server);
+		io.on('connection', socket => {
+			console.log('Client connected');
+		})
+	})
+	.catch((err) => console.log(err));
